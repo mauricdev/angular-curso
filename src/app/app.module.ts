cf92342498c31d370d4124dcaf72bbd6d53b4a18
mauricdev <49356191+mauricdev@users.noreply.github.com>
2021-04-26 22:23:52 -0400

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroesModule } from './heroes/heroes.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContadorComponent
+  ],
+  imports: [
+    BrowserModule,
+    HeroesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
